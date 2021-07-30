4bc8f6c262c1a4bbe100f9a3c61eeffc4871fba0
Isaac Mugomba <64057817+Mike909090@users.noreply.github.com>
2021-07-30 00:33:12 -0500

    Create inputs.kt

---
+++ inputs.kt
@@ -0,0 +1,16 @@
+fun main(args : Array<String>){
+
+ //Inputs
+        //Simple input
+        println("How are you doing")
+        var response = readLine()
+        println("I am doing $response")
+        println("How are you doing")
+        
+        //Advanced input
+        println("What bout now")
+        val keyboard = Scanner(System.`in`)
+        var secondndresponse = keyboard.nextLine()
+        println("Im glad you are $secondndresponse")
+
+    }
