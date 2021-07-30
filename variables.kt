9d2fecb30c981fb5cbf6a802ea24c76f939033ab
Isaac Mugomba <64057817+Mike909090@users.noreply.github.com>
2021-07-30 00:28:36 -0500

    Create variables.kt

---
+++ variables.kt
@@ -0,0 +1,25 @@
+//variables
+var firstName: String = "Isaac"
+var age: Int = 19
+var truth: Boolean = true
+
+//Immutable value
+val secondName = "Mike"
+
+//Mutable value
+var tShirt = "Polo"
+//Tried to assign variable but got an error???????
+//tShirt  "Old Navy"
+
+//String
+var name: String= "John"
+//Boolean
+val letter: Boolean= true
+//Byte
+val num1: Byte = 99
+//Short
+val num2: Short = 29999
+// int
+val num3: Int = 123456
+//Double
+val num4: Double= 10.55
